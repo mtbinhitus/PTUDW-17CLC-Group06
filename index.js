570930d2e2110d60ee611b082e39b656ea0992e4
Aspirin <1753030@student.hcmus.edu.vn>
2019-11-25 21:20:38 +0700

    Create Express Server

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("Hello World!!!");
+const express = require('express');
+const app = express();
+
+// set static folder
+app.use(express.static(__dirname + '/websites'));
+
+// start server
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), () => {
+    console.log(`server is listening on port ${app.get('port')}`);
+});
