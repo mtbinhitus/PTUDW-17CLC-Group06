afa7d4d72d0738cbfb59601236557b9c459d95f3
Aspirin <1753030@student.hcmus.edu.vn>
2019-11-25 21:39:50 +0700

    Format project and future

--- index.js
+++ index.js
@@ -3,10 +3,21 @@ const express = require('express');
 const app = express();
 
 // set static folder
-app.use(express.static(__dirname + '/websites'));
+app.use(express.static(__dirname + '/public'));
+// use handlebars
+const hbs = require('express-handlebars');
+app.engine('hbs', hbs({
+    extname: 'hbs',
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials',
+    defaultLayout: 'layout'
+}));
+app.set('view engine', 'hbs');
 
 // start server
 app.set('port', process.env.PORT || 3000);
-app.listen(app.get('port'), () => {
+const server = app.listen(app.get('port'), () => {
+    var host = server.address().address;
+    var port = server.address().port;
     console.log(`server is listening on port ${app.get('port')}`);
 });
