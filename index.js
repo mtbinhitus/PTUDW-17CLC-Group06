8ef1a42dd2d825a3e21e42541688413fc4515d0f
1753021 <1753021@student.hcmus.edu.vn>
2019-12-21 17:57:15 +0700

    Front-end part 1

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
 console.log("Hello World!!!");
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
 
-// set static folder
+// Setup static path use css and image
 app.use(express.static(__dirname + '/public'));
-// use handlebars
-const hbs = require('express-handlebars');
-app.engine('hbs', hbs({
-    extname: 'hbs',
-    layoutsDir: __dirname + '/views/layouts',
-    partialsDir: __dirname + '/views/partials',
-    defaultLayout: 'layout'
-}));
 
-app.set('views', __dirname + '/views');
+// Setup handlebars
+// Use View Engine
+var expressHbs = require('express-handlebars');
+var paginateHelper = require('express-handlebars-paginate');
+var hbs = expressHbs.create({
+	extname			: 'hbs',
+	defaultLayout	: 'layout', 
+	layoutsDir		: __dirname + '/views/layouts/',
+	partialsDir		: __dirname + '/views/partials/',
+	helpers			: {
+		paginate: paginateHelper.createPagination
+	}
+});
+app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 
+// Define your routes here
+// Homepage
 app.get("/", (req, res) => {
     let css = "index-styles.css";
     res.render('index', {css: css});
 });
 
-// start server
-app.set('port', process.env.PORT || 3000);
-const server = app.listen(app.get('port'), () => {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log(`server is listening on port ${app.get('port')}`);
+// Set Server Port & Start Server
+app.set('port', (process.env.PORT || 3000));
+
+app.listen(app.get('port'), function(){
+	console.log('Server is listening at port ' + app.get('port'));
 });
