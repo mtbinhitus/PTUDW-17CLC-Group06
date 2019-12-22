dbe08a704414630980ad9f8e143f75a01040b0a2
Binh Trinh <1753033@student.hcmus.edu.vn>
2019-12-22 13:05:19 +0700

    Sample data

--- seeders/20191221164236-demo-user.js
+++ seeders/20191221164236-demo-user.js
@@ -3,28 +3,57 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
    
-   let data = [
-     {
-      Pass: "123456",
-      FName: "Trinh",
-      LName: "Binh",
-      Email: "1753033@student.hcmus.edu.vn",
-      Avatar: "None",
-      isAdmin: "false",
-      Token: "",
-      Status: "true",
-     },
-     {
-      Pass: "000000",
-      FName: "Mai",
-      LName: "Binh",
-      Email: "1753030@student.hcmus.edu.vn",
-      Avatar: "None",
-      isAdmin: "true",
-      Token: "",
-      Status: "true",
-     }
-   ]
+   let data = [{
+    "id": 1,
+    "password": "7BLksPup",
+    "fname": "Norris",
+    "lname": "Drinkhill",
+    "email": "ndrinkhill0@addthis.com",
+    "avatar": null,
+    "isadmin": false,
+    "token": null,
+    "status": true
+  }, {
+    "id": 2,
+    "password": "xBrcwpa",
+    "fname": "Albertina",
+    "lname": "Jerche",
+    "email": "ajerche1@tiny.cc",
+    "avatar": null,
+    "isadmin": true,
+    "token": null,
+    "status": true
+  }, {
+    "id": 3,
+    "password": "2PgGRkJ2z2p",
+    "fname": "Isador",
+    "lname": "Overnell",
+    "email": "iovernell2@bloglines.com",
+    "avatar": null,
+    "isadmin": true,
+    "token": null,
+    "status": false
+  }, {
+    "id": 4,
+    "password": "Q7WCtAhd",
+    "fname": "Olivero",
+    "lname": "Shoulders",
+    "email": "oshoulders3@latimes.com",
+    "avatar": null,
+    "isadmin": true,
+    "token": null,
+    "status": true
+  }, {
+    "id": 5,
+    "password": "qb2hwgUJfTh9",
+    "fname": "Linell",
+    "lname": "Ewbank",
+    "email": "lewbank4@mediafire.com",
+    "avatar": null,
+    "isadmin": true,
+    "token": null,
+    "status": true
+  }]
    data.map(item => {
     item.createdAt = Sequelize.literal('NOW()');
     item.updatedAt = Sequelize.literal('NOW()');
--- seeders/20191221165946-demo-book.js
+++ seeders/20191221165946-demo-book.js
@@ -3,26 +3,34 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     
-    let data = [
-      {
-        ISBN13: "123",
-        ISBN10: "456",
-        Title: "Khong biet gi luon huhu",
-        Edition: "1",
-        PubYear: "22/12/2019",
-        Publisher: "Trinh Binh",
-        Amount: "22"
-      },
-      {
-        ISBN13: "123",
-        ISBN10: "456",
-        Title: "Khong biet gi luon huhu",
-        Edition: "1",
-        PubYear: "22/12/2019",
-        Publisher: "Trinh Binh",
-        Amount: "22"
-      }
-    ]
+    let data = [{
+      "id": 1,
+      "isbn13": "217159685-6",
+      "isbn10": "415977316-8",
+      "title": "porttitor lorem id ligula suspendisse ornare consequat lectus in est risus",
+      "edition": 28,
+      "pubYear": "2/26/2019",
+      "publisher": "luctus et ultrices posuere cubilia curae donec pharetra magna vestibulum aliquet ultrices erat tortor sollicitudin mi",
+      "amount": 29
+    }, {
+      "id": 2,
+      "isbn13": "343364111-0",
+      "isbn10": "462121316-4",
+      "title": "nec sem duis aliquam convallis nunc proin at turpis a pede posuere nonummy integer non velit donec",
+      "edition": 82,
+      "pubYear": "4/20/2019",
+      "publisher": "eros viverra eget congue eget semper rutrum nulla nunc purus phasellus in felis donec semper sapien a libero",
+      "amount": 8
+    }, {
+      "id": 3,
+      "isbn13": "179751315-X",
+      "isbn10": "429568962-9",
+      "title": "justo sollicitudin ut suscipit a feugiat et eros vestibulum ac est",
+      "edition": 29,
+      "pubYear": "10/23/2019",
+      "publisher": "maecenas ut massa quis augue luctus tincidunt nulla mollis molestie",
+      "amount": 10
+    }]
     data.map(item => {
      item.createdAt = Sequelize.literal('NOW()');
      item.updatedAt = Sequelize.literal('NOW()');
