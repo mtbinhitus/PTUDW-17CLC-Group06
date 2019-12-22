ef496e32ea97325ed0e15796e8f6c615bf8c4c24
1753021 <1753021@student.hcmus.edu.vn>
2019-12-22 10:12:09 +0700

    Dynamic web

--- index.js
+++ index.js
@@ -37,9 +37,47 @@ app.get("/", (req, res) => {
     res.render('index', {css: css});
 });
 
+// Search page
 app.get("/search", (req, res) => {
 	let css = "./public/css/search.css";
 	res.render('search', {css: css});
+});
+
+// MEMBER
+// Manage book
+app.get("membermanagebook", (req, res) => {
+	let css = "./public/css/member-manage-book.css";
+	res.render('membermanagebook', {css: css});
+});
+// Dashboard
+app.get("memberdashboard", (req, res) => {
+	let css = "./public/css/member-dashboard.css";
+	res.render('memberdashboard', {css: css});
+});
+
+// ADMIN
+// Add author
+app.get("/addauthor", (req, res) => {
+	let css = "./public/css/add-author.css";
+	res.render('addauthor', {css: css});
+});
+
+// Add category
+app.get("/addcategory", (req, res) => {
+	let css = "./public/css/add-category.css";
+	res.render('addcategory', {css: css});
+});
+
+// Manage category
+app.get("/managecategory", (req, res) => {
+	let css = "./public/css/manage-category.css";
+	res.render('managecategory', {css: css});
+});
+
+// Manage author
+app.get("/manageauthor", (req, res) => {
+	let css = "./public/css/manage-author.css";
+	res.render('manageauthor', {css: css});
 });
 
 // Set Server Port & Start Server
