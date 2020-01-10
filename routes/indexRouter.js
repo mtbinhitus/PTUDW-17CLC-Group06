79d0f113eb401b26554403c22025d72c7a579eab
Aspirin <1753030@student.hcmus.edu.vn>
2020-01-10 08:00:01 +0700

    Change homepage

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,18 +1,22 @@
 let express = require('express');
 let router = express.Router();
 
-router.get('/', (req, res, next) => {
-    let css = "./public/index-styles.css";
-    let bookController = require("../controllers/bookController");
-		bookController
-		.getAll()
-		.then(data => {
+router.get('/', (req, res) => {
+	let css = "./public/index-styles.css";
+	let bookController = require("../controllers/bookController");
+	bookController.getAll().then(data => {
 		res.locals.books = data.rows;
-		res.render('index', {css: css});
 		
+		let categoryController = require("../controllers/categoryController");
+		return categoryController.getAll();
+	}).then(data => {
+		res.locals.categories = data.rows;
+		res.render('index', {css: css});
+		console.log(res.locals.books);
 	})
-	.catch(error => next(error));
-   
- });
-
+	.catch(error => {
+		next(error);
+	});
+});
+ 
 module.exports = router;
